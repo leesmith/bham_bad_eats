01bd96ea49a694acc94859399898e6e833199534
Lee Smith <jeremyleesmith@gmail.com>
2017-04-04 00:39:55 -0500

    Update script to match website updates

--- bham_bad_eats.rb
+++ bham_bad_eats.rb
@@ -8,7 +8,7 @@ require 'csv'
 require 'open-uri'
 
 puts "========== #{Time.now} Start =========="
-DOC_ROOT = 'http://www.jcdh.org/EH/FnL/'
+DOC_ROOT = 'https://webapps.jcdh.org/scores/ehfs/'
 
 # Define inspection struct
 InspectionReport = Struct.new(:inspection_date, :score, :establishment, :link, :permit_number, :inspection_number) do
@@ -38,21 +38,23 @@ CSV.foreach('history.csv') do |row|
   inspection_history << InspectionReport.new(inspection_date, score, establishment, link, permit_number, inspection_number)
 end
 
-doc = Nokogiri::HTML(open(DOC_ROOT + 'FnL03.aspx'))
-score_table = doc.search("table[@id='ctl00_BodyContent_gvFoodScores']")
+doc = Nokogiri::HTML(open(DOC_ROOT + 'foodservicescores.aspx'))
+score_table = doc.search("table[@id='MainContent_gvFoodScores']")
 rows = score_table.search('tr')
 
 inspections = []
 rows.each_with_index do |tr, i|
   # skip header row
   next if i == 0
+  # skip footer row
+  break if i == (rows.size - 2)
 
-  score = tr.children[5].text.to_i
-  inspection_date = Date.strptime(tr.children[6].text, '%m/%d/%Y')
-  establishment = tr.children[2].text
-  permit_number = tr.children[5].search('a').attribute('href').value[/PermitNbr=\d+/].split('=').last.to_i
-  inspection_number = tr.children[5].search('a').attribute('href').value[/InspNbr=\d+/].split('=').last.to_i
-  link = "#{DOC_ROOT}FnL04.aspx?PermitNbr=#{permit_number}&InspNbr=#{inspection_number}"
+  score = tr.children[1].text.to_i
+  permit_number = tr.children[1].search('a').attribute('href').value[/PermitNbr=\d+/].split('=').last.to_i
+  inspection_number = tr.children[1].search('a').attribute('href').value[/InspNbr=\d+/].split('=').last.to_i
+  link = DOC_ROOT + tr.children[1].search('a').attribute('href').value
+  establishment = tr.children[2].text.strip.gsub(/\s+/, ' ')
+  inspection_date = Date.strptime(tr.children[3].text, '%m/%d/%Y')
   inspections << InspectionReport.new(inspection_date, score, establishment, link, permit_number, inspection_number)
 end
 
